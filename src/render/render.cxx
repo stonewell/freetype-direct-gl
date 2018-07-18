5162ac20a93b3dae60714e0fb75003f6c5980c91
stonewell <jingnan.si@gmail.com>
2018-07-18 23:33:07 +0800

    cleanup render code

--- src/render/render.cxx
+++ src/render/render.cxx
@@ -11,16 +11,27 @@ namespace render {
 namespace impl {
 class RenderImpl : public Render {
 public:
-    RenderImpl()
-        : m_Program{CreateRenderProgram()} {
+    RenderImpl() {
+        Init();
     }
 
-    virtual ~RenderImpl() {}
+    virtual ~RenderImpl() {
+        Destroy();
+    }
 
     virtual bool RenderText(text::TextBufferPtr text_buf);
 
 private:
     ProgramPtr m_Program;
+	GLuint m_Vertexbuffer;
+    GLuint m_RenderTextureIndex;
+    GLuint m_ColorIndex;
+    GLuint m_Position2Index;
+    GLuint m_RectIndex;
+
+private:
+    void Init();
+    void Destroy();
 };
 
 static
@@ -28,46 +39,62 @@ const GLfloat screen_quad[] = {
     -1, -1, 1, -1, -1, 1, 1, 1
 };
 
-bool RenderImpl::RenderText(text::TextBufferPtr text_buf) {
-    GLuint render_texture = text_buf->GetTexture();
+void RenderImpl::Init() {
+    m_Program = CreateRenderProgram();
 
-    auto c = glm::vec4(0.0, 0.0, 0.0, 0.0);
-    auto rect = glm::vec4(0.0, 0.0, 1.0, 1.0);
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_ZERO, GL_SRC_COLOR);
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glGenBuffers(1, &m_Vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(screen_quad),
                  screen_quad, GL_STATIC_DRAW);
 
     glUseProgram(*m_Program);
 
-    GLint posAttrib = glGetAttribLocation(*m_Program, "position2");
-    glEnableVertexAttribArray(posAttrib);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    m_Position2Index = glGetAttribLocation(*m_Program, "position2");
+
+    m_RenderTextureIndex = glGetUniformLocation(*m_Program, "texture");
+    m_ColorIndex = glGetUniformLocation(*m_Program, "color");
+    m_RectIndex = glGetUniformLocation(*m_Program, "rect");
+
+    glUseProgram(0);
+}
+
+void RenderImpl::Destroy() {
+    glDeleteBuffers(1, &m_Vertexbuffer);
+}
+
+bool RenderImpl::RenderText(text::TextBufferPtr text_buf) {
+    auto c = glm::vec4(0.0, 0.0, 0.0, 0.0);
+    const auto rect = glm::vec4(0.0, 0.0, 1.0, 1.0);
+
+    GLuint render_texture = text_buf->GetTexture();
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ZERO, GL_SRC_COLOR);
+
+    glUseProgram(*m_Program);
+	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
+
+    glEnableVertexAttribArray(m_Position2Index);
+    glVertexAttribPointer(m_Position2Index, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glUniform4fv(m_ColorIndex, 1, &c[0]);
+    glUniform4fv(m_RectIndex, 1, &rect[0]);
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, render_texture);
-    glUniform1i(glGetUniformLocation(*m_Program, "texture"), 0);
-
-    glUniform4fv(glGetUniformLocation(*m_Program, "color"),
-                 1, &c[0]);
-    glUniform4fv(glGetUniformLocation(*m_Program, "rect"),
-                 1, &rect[0]);
+    glUniform1i(m_RenderTextureIndex, 0);
 
     glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(screen_quad) / sizeof(GLfloat) / 2);
 
     glBlendFunc(GL_ONE, GL_ONE);
+
     c = glm::vec4(.0, .0, 1.0, 1.0);
-    glUniform4fv(glGetUniformLocation(*m_Program, "color"),
-                 1, &c[0]);
+
+    glUniform4fv(m_ColorIndex, 1, &c[0]);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(screen_quad) / sizeof(GLfloat) / 2);
 
-    glDisableVertexAttribArray(posAttrib);
+    glDisableVertexAttribArray(m_Position2Index);
+
     glUseProgram(0);
     return true;
 }
