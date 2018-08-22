939ad107a87345df7c49d757b58a10d71d9be755
stonewell <jingnan.si@gmail.com>
2018-08-22 08:07:29 +0800

    refactor render code

--- src/render/render.cxx
+++ src/render/render.cxx
@@ -34,10 +34,14 @@ private:
     GLuint m_ColorBackgroundIndex;
     GLuint m_Position2BackgroundIndex;
     GLuint m_RectBackgroundIndex;
+    GLuint m_VertexArray;
 
 private:
     void Init();
     void Destroy();
+
+    void DrawBackground(text::TextBufferPtr text_buf);
+    void DrawForeground(text::TextBufferPtr text_buf);
 };
 
 static
@@ -51,6 +55,8 @@ const GLfloat screen_quad[] = {
 void RenderImpl::Init() {
     m_Program = CreateRenderProgram();
     m_ProgramBackground = CreateRenderBackgroundProgram();
+
+    glGenVertexArraysAPPLE(1, &m_VertexArray);
 
 	glGenBuffers(1, &m_Vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
@@ -78,79 +84,94 @@ void RenderImpl::Init() {
 
 void RenderImpl::Destroy() {
     glDeleteBuffers(1, &m_Vertexbuffer);
+    glDeleteVertexArraysAPPLE(1, &m_VertexArray);
+}
+
+void RenderImpl::DrawBackground(text::TextBufferPtr text_buf) {
+	auto c = glm::vec4(0.0, 0.0, 0.0, 0.0);
+	auto rect = glm::vec4(0.0, 0.0, 1., 1.0);
+	auto count = text_buf->GetTextAttrCount();
+	auto text_attr = text_buf->GetTextAttr();
+
+	//draw background
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glUseProgram (*m_ProgramBackground);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
+    glEnableVertexAttribArray (m_Position2BackgroundIndex);
+	glVertexAttribPointer(m_Position2BackgroundIndex, 2, GL_FLOAT, GL_FALSE, 0,
+                          0);
+	for (uint32_t i = 0; i < count; i++, text_attr++) {
+		if (text_attr->back_color[3] == 0)
+			continue;
+
+		c = glm::vec4(text_attr->back_color[0], text_attr->back_color[1],
+                      text_attr->back_color[2], text_attr->back_color[3]);
+		rect = glm::vec4(text_attr->bounds[0], text_attr->bounds[1],
+                         text_attr->bounds[2], text_attr->bounds[3]);
+		glUniform4fv(m_ColorBackgroundIndex, 1, &c[0]);
+		glUniform4fv(m_RectBackgroundIndex, 1, &rect[0]);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0,
+                     sizeof(screen_quad) / sizeof(GLfloat) / 2);
+	}
+
+    glUseProgram(0);
+}
+
+void RenderImpl::DrawForeground(text::TextBufferPtr text_buf) {
+	auto c = glm::vec4(0.0, 0.0, 0.0, 0.0);
+	auto rect = glm::vec4(0.0, 0.0, 1., 1.0);
+	auto count = text_buf->GetTextAttrCount();
+	auto text_attr = text_buf->GetTextAttr();
+
+	//draw foreground
+	GLuint render_texture = text_buf->GetTexture();
+
+	glBlendFunc(GL_ZERO, GL_SRC_COLOR);
+	glUseProgram (*m_Program);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
+	glEnableVertexAttribArray (m_Position2Index);
+	glVertexAttribPointer(m_Position2Index, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	c = glm::vec4(0.0, 0.0, 0.0, 0.0);
+	rect = glm::vec4(0.0, 0.0, 1., 1.0);
+
+	glUniform4fv(m_ColorIndex, 1, &c[0]);
+	glUniform4fv(m_RectIndex, 1, &rect[0]);
+	glActiveTexture (GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, render_texture);
+	glUniform1i(m_RenderTextureIndex, 0);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0,
+                 sizeof(screen_quad) / sizeof(GLfloat) / 2);
+
+	glBlendFunc(GL_ONE, GL_ONE);
+
+	for (uint32_t i = 0; i < count; i++, text_attr++) {
+		c = glm::vec4(text_attr->color[0], text_attr->color[1],
+                      text_attr->color[2], text_attr->color[3]);
+		rect = glm::vec4(text_attr->bounds[0], text_attr->bounds[1],
+                         text_attr->bounds[2], text_attr->bounds[3]);
+		glUniform4fv(m_ColorIndex, 1, &c[0]);
+		glUniform4fv(m_RectIndex, 1, &rect[0]);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0,
+                     sizeof(screen_quad) / sizeof(GLfloat) / 2);
+	}
 }
 
 bool RenderImpl::RenderText(text::TextBufferPtr text_buf) {
-    auto c = glm::vec4(0.0, 0.0, 0.0, 0.0);
-    auto rect = glm::vec4(0.0, 0.0, 1., 1.0);
-    auto count = text_buf->GetTextAttrCount();
-    auto text_attr = text_buf->GetTextAttr();
+	glEnable (GL_BLEND);
 
-    glEnable(GL_BLEND);
-
-    //draw background
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    glUseProgram(*m_ProgramBackground);
-	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
-
-    glEnableVertexAttribArray(m_Position2BackgroundIndex);
-    glVertexAttribPointer(m_Position2BackgroundIndex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    for(uint32_t i = 0;i < count; i++, text_attr++) {
-        if (text_attr->back_color[3] == 0)
-            continue;
-
-        c = glm::vec4(text_attr->back_color[0], text_attr->back_color[1], text_attr->back_color[2], text_attr->back_color[3]);
-        rect = glm::vec4(text_attr->bounds[0], text_attr->bounds[1], text_attr->bounds[2], text_attr->bounds[3]);
-
-        glUniform4fv(m_ColorBackgroundIndex, 1, &c[0]);
-        glUniform4fv(m_RectBackgroundIndex, 1, &rect[0]);
-
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(screen_quad) / sizeof(GLfloat) / 2);
-    }
-
-    //draw foreground
-    GLuint render_texture = text_buf->GetTexture();
-
-    glBlendFunc(GL_ZERO, GL_SRC_COLOR);
-
-    glUseProgram(*m_Program);
-	glBindBuffer(GL_ARRAY_BUFFER, m_Vertexbuffer);
-
-    glEnableVertexAttribArray(m_Position2Index);
-    glVertexAttribPointer(m_Position2Index, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    c = glm::vec4(0.0, 0.0, 0.0, 0.0);
-    rect = glm::vec4(0.0, 0.0, 1., 1.0);
-
-    glUniform4fv(m_ColorIndex, 1, &c[0]);
-    glUniform4fv(m_RectIndex, 1, &rect[0]);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, render_texture);
-    glUniform1i(m_RenderTextureIndex, 0);
-
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(screen_quad) / sizeof(GLfloat) / 2);
-
-    glBlendFunc(GL_ONE, GL_ONE);
-
-    text_attr = text_buf->GetTextAttr();
-    for(uint32_t i = 0;i < count; i++, text_attr++) {
-        c = glm::vec4(text_attr->color[0], text_attr->color[1], text_attr->color[2], text_attr->color[3]);
-        rect = glm::vec4(text_attr->bounds[0], text_attr->bounds[1], text_attr->bounds[2], text_attr->bounds[3]);
-
-        glUniform4fv(m_ColorIndex, 1, &c[0]);
-        glUniform4fv(m_RectIndex, 1, &rect[0]);
-
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(screen_quad) / sizeof(GLfloat) / 2);
-    }
+	DrawBackground(text_buf);
+	DrawForeground(text_buf);
 
     glDisableVertexAttribArray(m_Position2Index);
+    glDisableVertexAttribArray(m_Position2BackgroundIndex);
 
     glUseProgram(0);
     return true;
 }
+
 } //namespace impl
 
 RenderPtr CreateRender() {
--- test/markup.cxx
+++ test/markup.cxx
@@ -149,8 +149,8 @@ int main( int argc, char **argv )
     glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
 	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow( 500, 220, argv[0], NULL, NULL );
 
